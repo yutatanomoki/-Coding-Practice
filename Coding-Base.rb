8df7ff6f4849058200eab20a098fbcb33bb2e5a5
yutatanomoki <74037339+yutatanomoki@users.noreply.github.com>
2021-11-23 20:45:33 +0900

    Update Coding-Base.rb

--- Coding-Base.rb
+++ Coding-Base.rb
@@ -115,3 +115,29 @@ while line = gets
 end
 p letter_A
 
+# 2次元配列で画像を表示する
+
+# 画像用配列
+players_img = [
+    "https://paiza-webapp.s3.amazonaws.com/files/learning/rpg/Empty.png",
+    "https://paiza-webapp.s3.amazonaws.com/files/learning/rpg/Dragon.png",
+    "https://paiza-webapp.s3.amazonaws.com/files/learning/rpg/Crystal.png",
+    "https://paiza-webapp.s3.amazonaws.com/files/learning/rpg/Hero.png",
+    "https://paiza-webapp.s3.amazonaws.com/files/learning/rpg/Heroine.png"]
+
+# 配置データを読み込み
+team = []
+while line = gets
+    line.chomp!
+    team.push(line.split(","))
+end
+
+# ここから先を入力してください
+print "<table>"
+team.each do |line|
+    print "<tr>"
+    line.each do |person|
+        print "<td><img src='#{players_img[person.to_i]}'></td>"
+    end
+    puts "</tr>"
+end
